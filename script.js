652802fe9896baa59b06e6df52222b999ddc128c
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-03-10 14:30:43 +0300

    fixed slider when scaled

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let offset = 0; //смещение от левого края
+let sliderN = 1; // номер страницы
 const sliderLine = document.querySelector(".repair__slider-line");
 const sliderWindow = document.querySelector(".repair__slider-window");
 const arrowLeft = document.querySelector('.arrow-left');
@@ -14,24 +15,11 @@ document.querySelector('.arrow-left').addEventListener("click", function (event)
     event.preventDefault();
     sliderWidth = sliderLine.offsetWidth;
 
-    console.log("Left line: " + sliderLine.getBoundingClientRect().left);
-    console.log("Left window: " + sliderLine.children[0].getBoundingClientRect().left);
-    console.log("Right line: " + sliderLine.getBoundingClientRect().right);
-    console.log("Right window: " + sliderLine.children[0].getBoundingClientRect().right);
-    console.log("width: " + sliderLine.offsetWidth);
-
-    // if (sliderLine.getBoundingClientRect().left - sliderLine.offsetWidth < 0) {
-    //     sliderWidth = sliderLine.offsetWidth;
-    //     offset = offset - sliderWidth;
-    //     sliderLine.style.left = -offset + 'px';
-
-    //     arrowRight.children[0].style.display = 'block';
-    // }
-
     if (sliderLine.getBoundingClientRect().left < 0) {
         sliderWidth = sliderLine.offsetWidth;
         offset = offset - sliderWidth;
         sliderLine.style.left = -offset + 'px';
+        sliderN--;
 
         arrowRight.children[0].style.display = 'block';
     }
@@ -40,35 +28,16 @@ document.querySelector('.arrow-left').addEventListener("click", function (event)
     } else {
         arrowLeft.children[0].style.display = 'block';
     }
-    // // else if (sliderLine.getBoundingClientRect().right + sliderLine.offsetWidth < 30) {
-    // //     arrowRight.children[0].style.display = 'none';
-    // // } 
-    // else if (sliderLine.getBoundingClientRect().right <= sliderWindow.getBoundingClientRect().left + 20) {
-    //     arrowRight.children[0].style.display = 'none';
-    // }
-    // else {
-    //     arrowRight.children[0].style.display = 'block';
-    // }
-
-    // if (sliderLine.getBoundingClientRect().left >= sliderWindow.getBoundingClientRect().left - 20) {
-    //     arrowLeft.children[0].style.display = 'none';
-    // }
-
-
 });
 
 document.querySelector('.arrow-right').addEventListener("click", function (event) {
     event.preventDefault();
-    console.log("Left line: " + sliderLine.getBoundingClientRect().left);
-    console.log("Left window: " + sliderLine.children[0].getBoundingClientRect().left);
-    console.log("Right line: " + sliderLine.getBoundingClientRect().right);
-    console.log("Right window: " + sliderLine.children[0].getBoundingClientRect().right);
-    console.log("width: " + sliderLine.offsetWidth);
 
     if (sliderLine.getBoundingClientRect().right + sliderLine.offsetWidth > 0) {
         sliderWidth = sliderLine.offsetWidth;
         offset = offset + sliderWidth;
         sliderLine.style.left = -offset + 'px';
+        sliderN++;
 
         arrowLeft.children[0].style.display = 'block';
     }
@@ -77,75 +46,10 @@ document.querySelector('.arrow-right').addEventListener("click", function (event
     } else {
         arrowRight.children[0].style.display = 'block';
     }
-
-    // else if (sliderLine.getBoundingClientRect().left >= sliderWindow.getBoundingClientRect().left - 20) {
-    //     arrowLeft.children[0].style.display = 'none';
-    // } else {
-    //     arrowLeft.children[0].style.display = 'block';
-    // }
-    // if (sliderLine.getBoundingClientRect().right <= sliderWindow.getBoundingClientRect().left + 20) {
-    //     arrowRight.children[0].style.display = 'none';
-    // }
-
 });
 
-
-
-// document.querySelector('.arrow-left').addEventListener("click", function (event) {
-//     event.preventDefault();
-//     sliderWidth = sliderLine.offsetWidth;
-
-//     console.log("Left: " + sliderLine.getBoundingClientRect().left);
-//     console.log("Right: " + sliderLine.getBoundingClientRect().right);
-
-//     if (sliderLine.getBoundingClientRect().left < -30) {
-//         sliderWidth = sliderLine.offsetWidth;
-//         offset = offset - sliderWidth;
-//         sliderLine.style.left = -offset + 'px';
-
-//         arrowRight.children[0].style.display = 'block';
-//     }
-//     // else if (sliderLine.getBoundingClientRect().right + sliderLine.offsetWidth < 30) {
-//     //     arrowRight.children[0].style.display = 'none';
-//     // }
-//     else if (sliderLine.getBoundingClientRect().right <= 20) {
-//         arrowRight.children[0].style.display = 'none';
-//     }
-//     else {
-//         arrowRight.children[0].style.display = 'block';
-//     }
-
-//     if (sliderLine.getBoundingClientRect().left >= -20) {
-//         arrowLeft.children[0].style.display = 'none';
-//     }
-//     console.log("cur Left: " + sliderLine.getBoundingClientRect().left);
-//     console.log("cur Right: " + sliderLine.getBoundingClientRect().right);
-
-
-// });
-
-// document.querySelector('.arrow-right').addEventListener("click", function (event) {
-//     event.preventDefault();
-//     console.log("Left: " + sliderLine.getBoundingClientRect().left);
-//     console.log("Right: " + sliderLine.getBoundingClientRect().right);
-
-//     if (sliderLine.getBoundingClientRect().right + sliderLine.offsetWidth > 30) {
-//         sliderWidth = sliderLine.offsetWidth;
-//         offset = offset + sliderWidth;
-//         sliderLine.style.left = -offset + 'px';
-
-//         arrowLeft.children[0].style.display = 'block';
-//     }
-//     else if (sliderLine.getBoundingClientRect().left >= 20) {
-//         arrowLeft.children[0].style.display = 'none';
-//     } else {
-//         arrowLeft.children[0].style.display = 'block';
-//     }
-//     if (sliderLine.getBoundingClientRect().right <= 20) {
-//         arrowRight.children[0].style.display = 'none';
-//     }
-//     console.log("cur Left: " + sliderLine.getBoundingClientRect().left);
-//     console.log("cur Right: " + sliderLine.getBoundingClientRect().right);
-
-// });
-
+window.addEventListener('resize', function () {
+    sliderWidth = sliderLine.offsetWidth;
+    offset = sliderWidth * (sliderN - 1);
+    sliderLine.style.left = -offset + 'px';
+});
